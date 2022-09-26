c4b2ffc699effd69a792d0b082dd4b4d4455ad1d
dream6648 <114422247+dream6648@users.noreply.github.com>
2022-09-26 23:40:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'cb-2lQ0cxEHtzV1QTPCxnAsd3CLYZj-o2vAfd8bp5Vw',
       // 所在省份
       province: '安徽',
       // 所在城市
